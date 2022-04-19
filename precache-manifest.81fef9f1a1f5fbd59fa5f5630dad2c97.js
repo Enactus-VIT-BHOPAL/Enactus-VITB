7b459cfe6ba24a5a192655f4821b826e0e125f66
Ananya Sharma <ananyasubodh8@gmail.com>
2022-04-19 12:03:12 +0530

    Updates

---
+++ precache-manifest.81fef9f1a1f5fbd59fa5f5630dad2c97.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a430ab9f3b44c78a3a57641337cd1a6",
+    "url": "/enactusvitb/index.html"
+  },
+  {
+    "revision": "c56cefa44d158e2b63db",
+    "url": "/enactusvitb/static/css/2.d43822b1.chunk.css"
+  },
+  {
+    "revision": "80df4cc2981ccb6f57f5",
+    "url": "/enactusvitb/static/css/main.8f3dc5b9.chunk.css"
+  },
+  {
+    "revision": "c56cefa44d158e2b63db",
+    "url": "/enactusvitb/static/js/2.88be0528.chunk.js"
+  },
+  {
+    "revision": "80df4cc2981ccb6f57f5",
+    "url": "/enactusvitb/static/js/main.b857140c.chunk.js"
+  },
+  {
+    "revision": "538ae5d7f5ed7660fb07",
+    "url": "/enactusvitb/static/js/runtime~main.61c73ff3.js"
+  },
+  {
+    "revision": "6d5542c50661df9c4bd081fe991a8824",
+    "url": "/enactusvitb/static/media/1.6d5542c5.jpg"
+  },
+  {
+    "revision": "76887ab5f831dd54f9b0e410d2898289",
+    "url": "/enactusvitb/static/media/Adya Pathak _Events Team member.76887ab5.jpg"
+  },
+  {
+    "revision": "2a2a868e88c960ee53d6138fd3a6fabd",
+    "url": "/enactusvitb/static/media/Anant_Dubey_Creative_team.2a2a868e.png"
+  },
+  {
+    "revision": "e76463439a6a7dad33fb35ac17ace6d0",
+    "url": "/enactusvitb/static/media/Ananya_Prasad.e7646343.jpeg"
+  },
+  {
+    "revision": "47ac7882c2b54f0e31f8ff41b4944665",
+    "url": "/enactusvitb/static/media/Animesh Anant_Project Team.47ac7882.jpg"
+  },
+  {
+    "revision": "d77f07b94cc695419e60f25877cf1cb9",
+    "url": "/enactusvitb/static/media/Anirudh Praveen Shamna - Finance Core Member.d77f07b9.jpg"
+  },
+  {
+    "revision": "6022a24bab0c2ecd4749a1747b6879ae",
+    "url": "/enactusvitb/static/media/Anupam Srivastava (creative team ).6022a24b.jpg"
+  },
+  {
+    "revision": "eebce51ab4da459954fe163a01987d52",
+    "url": "/enactusvitb/static/media/Areena Khan - Project Lead.eebce51a.jpeg"
+  },
+  {
+    "revision": "12e6653fa25af1b63b8f11c304e14f72",
+    "url": "/enactusvitb/static/media/Arun_Thomas_M_Creatives&Design Team Member.12e6653f.png"
+  },
+  {
+    "revision": "ed540a330bfe5fa876df46339af8a888",
+    "url": "/enactusvitb/static/media/Arushi Saxena-Marketing team_.ed540a33.webp"
+  },
+  {
+    "revision": "a8c11966edd5453fc8e4c9edd2b14089",
+    "url": "/enactusvitb/static/media/Ayush_Project Lead.a8c11966.jpg"
+  },
+  {
+    "revision": "0dd96b01b4f0862ca2321aa48033939c",
+    "url": "/enactusvitb/static/media/Divya Chakshu Bisht Tech team (1).0dd96b01.jpg"
+  },
+  {
+    "revision": "29c272a143447531a8bc2506f7bb6de7",
+    "url": "/enactusvitb/static/media/Fahad Usmani- Project Lead.29c272a1.JPG"
+  },
+  {
+    "revision": "723b25221fa34a673ef21139ccecaee7",
+    "url": "/enactusvitb/static/media/Harsha Khadatkar event team21BCE11147.723b2522.jpg"
+  },
+  {
+    "revision": "2cd4ad8d46416ae8f553351eca3fdc50",
+    "url": "/enactusvitb/static/media/Harshil Rathod - Technical Team.2cd4ad8d.jpg"
+  },
+  {
+    "revision": "107ffb0d9d1913b5cdc85752fdcb2f5a",
+    "url": "/enactusvitb/static/media/Isha Dwivedi - Creatives Lead.107ffb0d.png"
+  },
+  {
+    "revision": "e14f2474764eea4db265d59866ac9cf1",
+    "url": "/enactusvitb/static/media/Isha Singhal_project Team.e14f2474.jpg"
+  },
+  {
+    "revision": "d3da621a7323fdcccdcdb51b086a3809",
+    "url": "/enactusvitb/static/media/Khushi Sharma_Marketing team.d3da621a.jpeg"
+  },
+  {
+    "revision": "918600f3379e349e0a61f2bd7733e11b",
+    "url": "/enactusvitb/static/media/MANASWINI SINGH, Photography C0-Lead .918600f3.jpg"
+  },
+  {
+    "revision": "8389c6cf1d877b4c7104e7506c7baade",
+    "url": "/enactusvitb/static/media/Mahika Agrawal - Funding and Finance Lead.8389c6cf.jpeg"
+  },
+  {
+    "revision": "4bbbfd0473aac3fd00480ae75b41ac0d",
+    "url": "/enactusvitb/static/media/Mitrashis Basu Roy - Content team.4bbbfd04.jpg"
+  },
+  {
+    "revision": "09e60b776d000f8da0bc91c2faf9216e",
+    "url": "/enactusvitb/static/media/Mukta Soni_Events Team member.09e60b77.jpg"
+  },
+  {
+    "revision": "aef57391e9e872dfe96bdd36d469006a",
+    "url": "/enactusvitb/static/media/Muskan Pandey- Project lead.aef57391.jpg"
+  },
+  {
+    "revision": "19a41abb1260d43868ea20dc20a387f5",
+    "url": "/enactusvitb/static/media/NayanKumar_EventsLead.19a41abb.jpeg"
+  },
+  {
+    "revision": "120656e76f46e78c99b1f2160600d283",
+    "url": "/enactusvitb/static/media/Nitya Bansal Content Team.120656e7.jpg"
+  },
+  {
+    "revision": "285ad23de9b0304f30f67014816ca8cd",
+    "url": "/enactusvitb/static/media/Roohi Shaik- Project team.285ad23d.jpg"
+  },
+  {
+    "revision": "714256eda1ca834fd13b095ee1b6e9ce",
+    "url": "/enactusvitb/static/media/SRIJAN SAHU (PHOTOGRAPHY LEAD).714256ed.jpg"
+  },
+  {
+    "revision": "02b9ff1121e033cd2e01263be17aec73",
+    "url": "/enactusvitb/static/media/SUHANI SINGHAL_MARKETING TEAM.02b9ff11.jpeg"
+  },
+  {
+    "revision": "42e0c6e41c11e67a365741002ccbc708",
+    "url": "/enactusvitb/static/media/Sanskruti Dube-Treasurer.42e0c6e4.jpg"
+  },
+  {
+    "revision": "a074a7f1490652e9dc261a6f3812ef8e",
+    "url": "/enactusvitb/static/media/Saubhaggya Srivastava FInance Team.a074a7f1.jpg"
+  },
+  {
+    "revision": "b470360b4bc2ed41421d8499fced4087",
+    "url": "/enactusvitb/static/media/Siddhartha Sharma_Technical Co Lead.b470360b.png"
+  },
+  {
+    "revision": "8be3517e238e56bb4c93af72171cd73f",
+    "url": "/enactusvitb/static/media/Tejaswini Johri- Marketing Lead.8be3517e.jpg"
+  },
+  {
+    "revision": "972142bfe393dd262531716e889e615e",
+    "url": "/enactusvitb/static/media/Vaishali Jain_Technical Team.972142bf.JPG"
+  },
+  {
+    "revision": "091610d3b51a9a9ea132d339c5571f7b",
+    "url": "/enactusvitb/static/media/Zankhana Mehta-creative Co lead.091610d3.jpg"
+  },
+  {
+    "revision": "c172f16119779753768bf30dae3c4224",
+    "url": "/enactusvitb/static/media/ananya_sharma- Technical Team Lead.c172f161.jpeg"
+  },
+  {
+    "revision": "e849bba24d5e31d8707d8078c18508b8",
+    "url": "/enactusvitb/static/media/enactus-group-photo.e849bba2.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/enactusvitb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/enactusvitb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/enactusvitb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/enactusvitb/static/media/slick.f97e3bbf.svg"
+  }
+]);
